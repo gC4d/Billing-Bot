603e129c61a897a62170bb66c17e2e027d3dcd5e
gC4d <guilhermeacadorin@gmail.com>
2025-03-17 22:04:09 -0300

    Refactor CostumerBill model for improved readability by formatting field definitions

--- billing-bot/app/domain/models/costumer_bill.py
+++ billing-bot/app/domain/models/costumer_bill.py
@@ -3,5 +3,8 @@ from sqlmodel import Field, SQLModel
 
 
 class CostumerBill(SQLModel, table=True):
-    costumer_id : Optional[int] = Field(default=None, primary_key=True, foreign_key="costumer.id")
-    bill_id : Optional[int] = Field(default=None, primary_key=True, foreign_key="bill.id")
+    costumer_id : Optional[int] = Field(
+        default=None, primary_key=True, foreign_key="costumer.id")
+    
+    bill_id : Optional[int] = Field(
+        default=None, primary_key=True, foreign_key="bill.id")
