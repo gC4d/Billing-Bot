7e9b524dcc2e0a96c4483e736916c6f6399ebd2e
gC4d <guilhermeacadorin@gmail.com>
2025-03-17 21:18:53 -0300

    Add database session management with SQLModel

--- billing-bot/app/infraestructure/database/session.py
+++ billing-bot/app/infraestructure/database/session.py
@@ -4,6 +4,12 @@ from app.core.config import settings
 engine = create_engine(settings.sqlmodel_database_url, connect_args={"check_same_thread": False})
 
 def get_db():
+    """
+    Yields a database session.
+
+    This function creates a new SQLModel session using the provided engine and yields it.
+    It ensures that the session is properly closed after use.
+    """
     with Session(engine) as session:
         yield session
 
