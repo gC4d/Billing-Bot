4c5c6382b24293665b050df2e113d92a679b10b9
gC4d <guilhermeacadorin@gmail.com>
2025-03-19 23:17:17 -0300

    Refactor Bill model to enforce description length constraints and remove unused contact repository

--- billing-bot/app/domain/models/bill.py
+++ billing-bot/app/domain/models/bill.py
@@ -5,13 +5,22 @@ from sqlmodel import UUID, Field, Relationship, SQLModel
 from app.domain.models.costumer import Costumer
 from app.domain.models.costumer_bill import CostumerBill
 
+class FieldConfig:
+    DESCRIPTION_MAX = 200
+    DESCRIPTION_MIN = 10
+
 class Bill(SQLModel, table=True):
     
     id: UUID = Field(default=None, primary_key=True)
-    description: str = Field(default=None)
+    description: str = Field(
+        default=None, 
+        max_length=FieldConfig.DESCRIPTION_MAX,
+        min_length=FieldConfig.DESCRIPTION_MIN
+    )
     amount: float = Field(nullable=False)
-    payment_date: int = Field(nullable=False)
+    payment_date: int = Field(nullable=False,) # NORMALIZAR - IMPLEMENTAR UM MODELO QUE ARMAZENE AS DATAS DE PAGAMENTO
     billing_interval: int = Field(nullable=False)
     payment_cycles: int = Field(nullable=False)
     
     costumers : List[Costumer] = Relationship(back_populates="bill", link_model=CostumerBill)
+    
