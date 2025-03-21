d7a9a8c5607170ffc584295e37f0cf1804cba313
gC4d <guilhermeacadorin@gmail.com>
2025-03-20 23:05:58 -0300

    Refactor Bill model to improve field configuration and add payment schedule relationship

--- billing-bot/app/domain/models/bill.py
+++ billing-bot/app/domain/models/bill.py
@@ -4,23 +4,36 @@ from typing import List
 from sqlmodel import UUID, Field, Relationship, SQLModel
 from app.domain.models.costumer import Costumer
 from app.domain.models.costumer_bill import CostumerBill
+from app.domain.models.bill_payment_schedule import BillPaymentSchedule
 
-class FieldConfig:
+class BillFieldConfig:
     DESCRIPTION_MAX = 200
     DESCRIPTION_MIN = 10
+    
+    BILLING_MIN_INTERVAL = 1
+    BILLING_MAX_INTERVAL = 12
+    
+    PAYMENT_CYCLES_MIN = 1
 
 class Bill(SQLModel, table=True):
     
     id: UUID = Field(default=None, primary_key=True)
     description: str = Field(
         default=None, 
-        max_length=FieldConfig.DESCRIPTION_MAX,
-        min_length=FieldConfig.DESCRIPTION_MIN
+        max_length=BillFieldConfig.DESCRIPTION_MAX,
+        min_length=BillFieldConfig.DESCRIPTION_MIN
     )
     amount: float = Field(nullable=False)
-    payment_date: int = Field(nullable=False,) # NORMALIZAR - IMPLEMENTAR UM MODELO QUE ARMAZENE AS DATAS DE PAGAMENTO
-    billing_interval: int = Field(nullable=False)
-    payment_cycles: int = Field(nullable=False)
+    billing_interval: int = Field(
+        nullable=False,
+        min_length=BillFieldConfig.BILLING_MIN_INTERVAL,
+        max_length=BillFieldConfig.BILLING_MAX_INTERVAL
+    )
+    payment_cycles: int = Field(
+        nullable=False,
+        min_length=BillFieldConfig.PAYMENT_CYCLES_MIN
+    )
     
-    costumers : List[Costumer] = Relationship(back_populates="bill", link_model=CostumerBill)
-    
+    costumers: List[Costumer] = Relationship(back_populates="bill", link_model=CostumerBill)
+    payment_schedule: List[BillPaymentSchedule] = Relationship(back_populates="bill")
+
