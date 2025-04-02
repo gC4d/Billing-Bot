484a637cad8a7dd862ad66611a2b9827d57e9f56
gC4d <guilhermeacadorin@gmail.com>
2025-04-02 20:54:20 -0300

    Update get_costumers method to use correct Customer reference and add docstring

--- billing-bot/app/domain/repositories/bill_repository_interface.py
+++ billing-bot/app/domain/repositories/bill_repository_interface.py
@@ -8,8 +8,9 @@ from app.domain.repositories.base_repository import BaseRepository
 
 
 class IBillRepository(BaseRepository[Bill], ABC):
-
+    """
     @abstractmethod
     def get_costumers(self, id: int | UUID) -> List[Customer]:
-        """Retrieve all costumers by a bill's unique identifier."""
+       
         pass
+    """
--- billing-bot/app/infraestructure/repositories/bill_repository.py
+++ billing-bot/app/infraestructure/repositories/bill_repository.py
@@ -1,7 +1,6 @@
 from sqlmodel import UUID, Session, select
 from typing import List, Optional
 from app.domain.models.bill import Bill
-from app.domain.models.customer import Customer
 from app.domain.repositories.bill_repository_interface import IBillRepository
 
 
@@ -17,12 +16,12 @@ class BillRepository(IBillRepository):
     def get_all(self) -> List[Bill]:
         statement = select(Bill)
         return list(self.db.exec(statement).all())
-
+    """
     def get_costumers(self, id: int | UUID) -> List[Customer]:
         statement = select(Bill).where(Bill.id == id)
         bill = self.db.exec(statement).first()
-        return bill.costumers if bill else []
-
+        return bill.customers if bill else []
+    """
     def create(self, bill: Bill) -> Bill:
         self.db.commit()
         self.db.refresh(bill)
